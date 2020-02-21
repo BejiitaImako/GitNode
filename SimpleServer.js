0fe2761b262a8c70edf64d96f25bfd8306292e22
BejiitaImako <john.vegetasaiyajin@gmail.com>
2020-02-21 20:36:29 +0100

    Finished the server, added simple JSON stringify

--- SimpleServer.js
+++ SimpleServer.js
@@ -1,6 +1,13 @@
 const http = require('http');
 const port = 3000;
-const server = http.createServer(function(req, res) {});
+const server = http.createServer(function(req, res) {
+	let dataset = { Name: 'Bejiita', Occupation: 'Coder' };
+	let data = JSON.stringify(dataset);
+	res.setHeader('Content-type', 'application/JSON');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.writeHead(200);
+	res.end(data);
+});
 server.listen(port, function(err) {
 	if (err) {
 		console.log('Server start on port ' + port + ' fail');
